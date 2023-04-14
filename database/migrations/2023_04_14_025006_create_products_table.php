7cbfad69a78d62f9c80ee500f4fb4858cb8716a2
yayanrw <yayanraw@gmail.com>
2023-04-14 10:13:25 +0700

    add products migration

---
+++ database/migrations/2023_04_14_025006_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_code');
+            $table->string('product_name');
+            $table->string('category')->nullable();
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('price');
+            $table->integer('stock')->default(0);
+            $table->timestamps();
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('set null');
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
