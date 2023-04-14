97452c18957f7b8ba5f7b448ae8482f708621426
yayanrw <yayanraw@gmail.com>
2023-04-14 10:46:21 +0700

    add route to auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,29 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+// Public routes
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/register', 'register')->name('register');
+    Route::post('/login', 'login')->name('login');;
+});
+
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Products
+
+    // Banners
+});
+
+
+// 404 handler
+Route::fallback(function () {
+    return response()->json([
+        'status' => false,
+        'message' => 'Page not found'
+    ], 404);
 });
